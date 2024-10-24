6a25163b8a88650e7903ba10e15e85fd58e57e62
Gregorio Santos <gregoriohd@discovery.gregoriohd.com.br>
2024-10-24 19:50:03 -0300

    commit inicial

---
+++ src/main/java/br/com/gregoriohd/EscolaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gregoriohd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaApiApplication.class, args);
+	}
+
+}
