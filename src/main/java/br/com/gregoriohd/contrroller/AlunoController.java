4dbfa403a37ff682da4fb9e6e0b22a11b2b4893a
Gregorio Santos <gregoriohd@discovery.gregoriohd.com.br>
2024-10-26 21:16:02 -0300

    adicionando regras ao servico aluno e contruindo endpoit de aluno

--- src/main/java/br/com/gregoriohd/contrroller/AlunoController.java
+++ src/main/java/br/com/gregoriohd/contrroller/AlunoController.java
@@ -1,26 +1,65 @@
 package br.com.gregoriohd.contrroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gregoriohd.entity.Aluno;
 import br.com.gregoriohd.service.AlunoService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("alunos/")
+@RequestMapping("/alunos")
 public class AlunoController {
 
 	@Autowired
 	private AlunoService alunoService;
 
 	@PostMapping
-	public Aluno salvarAluno(@RequestBody Aluno aluno) {
-
+	public ResponseEntity<Aluno> salvarAluno(@RequestBody Aluno aluno) {
 		alunoService.salvar(aluno);
-		return aluno;
+        return ResponseEntity.status(HttpStatus.CREATED).body(aluno);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Aluno> obterAlunoPorId(@PathVariable Long id) {
+		Aluno aluno = alunoService.obterPorId(id);
+        if (aluno != null) {
+            return ResponseEntity.ok(aluno);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Aluno> atualizarAluno(@PathVariable Long id, @RequestBody Aluno aluno) {
+		Aluno alunoValid = alunoService.atualizar(id, aluno);
+        if (alunoValid != null) {
+            return ResponseEntity.ok(aluno);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> removerAluno(@PathVariable Long id) {
+		Aluno aluno = alunoService.obterPorId(id);
+		if (aluno != null) {
+			alunoService.remover(aluno);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
 	}
 
 }
--- src/main/java/br/com/gregoriohd/service/AlunoService.java
+++ src/main/java/br/com/gregoriohd/service/AlunoService.java
@@ -1,5 +1,7 @@
 package br.com.gregoriohd.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +19,31 @@ public class AlunoService {
 	public void salvar(Aluno aluno) {
 		alunoRepository.save(aluno);
 	}
+	
+	@Transactional(readOnly = true)
+	public Aluno obterPorId(Long id) {
+		return alunoRepository.findById(id).orElse(null);
+	}
+	
+	@Transactional(readOnly = false)
+	public Aluno atualizar(Long id, Aluno aluno) {
+		Optional<Aluno> alunoFind = alunoRepository.findById(id);
+		
+		if(!alunoFind.isPresent()) {
+			return null;
+		}
+		aluno.setId(alunoFind.get().getId());
+		salvar(aluno);
+		
+		return aluno;
+		
+	}
+	
+	@Transactional(readOnly = false)
+	public void remover(Aluno aluno) {
+		alunoRepository.delete(aluno);
+	}
+	
+	
 
 }
